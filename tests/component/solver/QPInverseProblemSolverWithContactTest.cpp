ae9e4dc7367be1700b8366b483b8ec7323452d66
EulalieCoevoet <eulalie.coevoet@gmail.com>
2025-03-31 13:08:15 +0200

    [tests] fixes QPInverseProblemSolverWithContactTest

--- tests/component/solver/QPInverseProblemSolverWithContactTest.cpp
+++ tests/component/solver/QPInverseProblemSolverWithContactTest.cpp
@@ -5,25 +5,24 @@ using std::string ;
 #include <sofa/helper/BackTrace.h>
 #include <sofa/helper/system/Locale.h>
 
-#include <SofaPython/SceneLoaderPY.h>
-using sofa::simulation::SceneLoaderPY ;
+#include <SofaPython3/PythonEnvironment.h>
+#include <SofaPython3/SceneLoaderPY3.h>
+using sofapython3::SceneLoaderPY3 ;
 
-#include <SofaSimulationGraph/DAGSimulation.h>
+#include <sofa/simulation/graph/DAGSimulation.h>
 using sofa::simulation::Simulation ;
+#include <sofa/simulation/Node.h>
 using sofa::simulation::Node ;
-using sofa::simulation::setSimulation ;
 using sofa::core::objectmodel::New ;
 using sofa::core::objectmodel::BaseData ;
 
-#include "component/solver/QPInverseProblemSolver.h"
-using sofa::component::constraintset::QPInverseProblemSolver;
+#include <SoftRobots.Inverse/component/solver/QPInverseProblemSolver.h>
+using softrobotsinverse::solver::QPInverseProblemSolver;
 
-using sofa::helper::vector;
+using sofa::type::vector;
 using sofa::helper::rabs;
 using std::stof;
 using std::istringstream;
-
-#include <SofaPython/PythonEnvironment.h>
 
 
 namespace sofa
@@ -36,19 +35,16 @@ protected:
     typedef QPInverseProblemSolver ThisClass ;
 
     simulation::Node::SPtr m_root;                 ///< Root of the scene graph, created by the constructor an re-used in the tests
-    simulation::Simulation* m_simulation;          ///< created by the constructor an re-used in the tests
-
 
     void SetUp()
     {
-        sofa::simulation::PythonEnvironment::Init();
+        sofapython3::PythonEnvironment::Init();
 
         // sofa init
-        static const string scenePath  = string(SOFTROBOTS_TEST_DIR) + std::string("/component/solver/scenes/CubeOnFloor.pyscn");
-        sofa::simulation::setSimulation(m_simulation = new sofa::simulation::graph::DAGSimulation());
-        // load scene
-        m_root = simulation::getSimulation()->load(scenePath.c_str());
-        simulation::getSimulation()->init(m_root.get());
+        static const string scenePath  = string(SOFTROBOTSINVERSE_TEST_DIR) + std::string("/component/solver/scenes/CubeOnFloor.pyscn");
+        m_root = core::objectmodel::SPtr_dynamic_cast<simulation::Node>( sofa::simulation::node::load(scenePath.c_str()));
+
+        sofa::simulation::node::initRoot(m_root.get());
 
         if(!m_root)
             ADD_FAILURE() << "Error while loading the scene: " << scenePath << std::endl;
@@ -58,7 +54,7 @@ protected:
     void testFallingCube()
     {
         helper::system::TemporaryLocale locale(LC_NUMERIC, "C");
-        m_simulation->reset(m_root.get());
+        sofa::simulation::node::reset(m_root.get());
 
         int nbTimeStep = 10;
         m_root->getObject("QPInverseProblemSolver")->findData("epsilon")->read("0.0");
@@ -70,7 +66,7 @@ protected:
         m_root->getChild("goal")->getObject("goalMO")->findData("position")->read("10 0 0");
 
         for(int i=0; i<nbTimeStep; i++)
-            m_simulation->animate(m_root.get());
+            sofa::simulation::node::animate(m_root.get());
 
         string posString = m_root->getChild("model")->getChild("effector")->getObject("effectorPoint")->findData("position")->getValueString();
         float x, y, z;
@@ -82,7 +78,7 @@ protected:
     void testFallingCubeWithFriction()
     {
         helper::system::TemporaryLocale locale(LC_NUMERIC, "C");
-        m_simulation->reset(m_root.get());
+        sofa::simulation::node::reset(m_root.get());
 
         int nbTimeStep = 10;
         m_root->getObject("QPInverseProblemSolver")->findData("epsilon")->read("0.0");
@@ -93,7 +89,7 @@ protected:
         m_root->getChild("goal")->getObject("goalMO")->findData("position")->read("10 0 0");
 
         for(int i=0; i<nbTimeStep; i++)
-            m_simulation->animate(m_root.get());
+            sofa::simulation::node::animate(m_root.get());
 
         string posString = m_root->getChild("model")->getChild("effector")->getObject("effectorPoint")->findData("position")->getValueString();
         float x, y, z;
@@ -116,27 +112,27 @@ protected:
 
 
         for(int i=0; i<nbTimeStep; i++)
-            m_simulation->animate(m_root.get());
+            sofa::simulation::node::animate(m_root.get());
         string force1 = m_root->getChild("model")->getObject("act")->findData("force")->getValueString();
 
-        m_simulation->reset(m_root.get());
+        sofa::simulation::node::reset(m_root.get());
         m_root->getChild("goal")->getObject("goalMO")->findData("position")->read("11 0 0");
         m_root->getObject("CollisionResponse")->findData("responseParams")->read("mu=0.5");
         m_root->getObject("QPInverseProblemSolver")->findData("responseFriction")->read("0.5");
 
 
         for(int i=0; i<nbTimeStep; i++)
-            m_simulation->animate(m_root.get());
+            sofa::simulation::node::animate(m_root.get());
         string force2 = m_root->getChild("model")->getObject("act")->findData("force")->getValueString();
 
-        m_simulation->reset(m_root.get());
+        sofa::simulation::node::reset(m_root.get());
         m_root->getChild("goal")->getObject("goalMO")->findData("position")->read("11 0 0");
         m_root->getObject("CollisionResponse")->findData("responseParams")->read("mu=1.");
         m_root->getObject("QPInverseProblemSolver")->findData("responseFriction")->read("1.");
 
 
         for(int i=0; i<nbTimeStep; i++)
-            m_simulation->animate(m_root.get());
+            sofa::simulation::node::animate(m_root.get());
         string force3 = m_root->getChild("model")->getObject("act")->findData("force")->getValueString();
 
         EXPECT_LE(stof(force1),stof(force2));
@@ -149,7 +145,7 @@ protected:
 
         int nbTimeStep = 25;
 
-        m_simulation->reset(m_root.get());
+        sofa::simulation::node::reset(m_root.get());
         m_root->getObject("QPInverseProblemSolver")->findData("allowSliding")->read("1"); // Enable sliding
         m_root->getObject("CollisionResponse")->findData("responseParams")->read("mu=0.8");
         m_root->getObject("QPInverseProblemSolver")->findData("responseFriction")->read("0.8");
@@ -160,7 +156,7 @@ protected:
         {
             position += 2./nbTimeStep*i;
             m_root->getChild("goal")->getObject("goalMO")->findData("position")->read(std::to_string(position)+" 0 0");
-            m_simulation->animate(m_root.get());
+            sofa::simulation::node::animate(m_root.get());
         }
 
         string deltaString = m_root->getChild("model")->getChild("effector")->getObject("PositionEffector")->findData("delta")->getValueString();
@@ -168,7 +164,7 @@ protected:
         getValueFromString(deltaString, x, y, z);
         double norm1 = sqrt(x*x+y*y+z*z);
 
-        m_simulation->reset(m_root.get());
+        sofa::simulation::node::reset(m_root.get());
         m_root->getObject("QPInverseProblemSolver")->findData("allowSliding")->read("0"); // Disable sliding
         m_root->getChild("goal")->getObject("goalMO")->findData("position")->read("12 0 0");
         m_root->getObject("CollisionResponse")->findData("responseParams")->read("mu=0.8");
@@ -176,7 +172,7 @@ protected:
 
 
         for(int i=0; i<nbTimeStep; i++)
-            m_simulation->animate(m_root.get());
+            sofa::simulation::node::animate(m_root.get());
 
         deltaString = m_root->getChild("model")->getChild("effector")->getObject("PositionEffector")->findData("delta")->getValueString();
         getValueFromString(deltaString, x, y, z);
